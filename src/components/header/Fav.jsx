c49ff4e6dbf6b57930aa4d231c73ff6bca9fce2d
muhamash <muhammad-ashraful@outlook.com>
2024-05-30 23:41:38 +0600

    writing the basic jsx components

---
+++ src/components/header/Fav.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+import HeartIcon from '../../assets/heart.svg';
+
+export default function Fav({onShow}) {
+    return (
+        <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
+            <img src={ HeartIcon } alt="heart" />
+            <span onClick={ onShow }>Favorite Locations</span>
+        </div>
+    );
+}

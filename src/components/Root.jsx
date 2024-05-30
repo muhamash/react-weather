5d24512a0fa5bca5da17f260e71ddb94b8a1c26f
muhamash <muhammad-ashraful@outlook.com>
2024-05-31 01:27:25 +0600

    template done!

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,14 +1,16 @@
 // import React from 'react'
-import FavModal from './header/FavModal'
-import Header from './header/Header'
+import Header from './header/Header';
+import Board from './weather/Board';
 
 export default function Root() {
   return (
     <div className="grid place-items-center h-screen bg-no-repeat bg-cover">
-          <Header />
-          <main>
-              <FavModal/>
-          </main>
+      <Header />
+      <main>
+        {/* <FavModal/> */ }
+        {/* <AddFav/> */ }
+        <Board />
+      </main>
     </div>
-  )
+  );
 }
---
+++ src/components/weather/AddFav.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+export default function AddFav() {
+    return (
+        <div className="md:col-span-2">
+            <div className="flex items-center justify-end space-x-6">
+                <button className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D] cursor-pointer hover:bg-green-500 hover:text-white shadow-md">
+                    <span>Add to Favorite</span>
+                    <img src="./assets/heart.svg" alt="" />
+                </button>
+								
+            </div>
+        </div>
+    );
+}

4b266e0ad5711603c32259ac922aaabc2f709c7d
muhamash <muhammad-ashraful@outlook.com>
2024-06-03 02:54:33 +0600

    v2 style fixed

--- src/components/header/FavModal.jsx
+++ src/components/header/FavModal.jsx
@@ -2,21 +2,36 @@
 // import React from 'react';
 import { useFav } from '../hooks/useFav';
 import { useFetch } from '../hooks/useFetch';
-export default function FavModal ({onShow})
-{
+
+export default function FavModal({ onShow }) {
     const { fav } = useFav();
     const { setEndpoint } = useFetch();
-    console.log(fav)
+    console.log(fav);
+
+    const handleClick = (e) => {
+        e.stopPropagation();
+    };
+
     return (
-        <div onClick={onShow} className="max-w-xs py-4 bg-white mix-blend-multiply bg-opacity-70 rounded-md border-[1px] transition-all duration-150 ease-in-out border-sky-500 absolute right-0 top-16 text-black shadow-lg ">
-            <h3 className="text-lg font-mono text-rose-500 px-4">Favorite Locations</h3>
-            <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
-                { fav.map( ( f, index ) => (
-                    <li
-                        onClick={()=>setEndpoint(f)}
-                        className="hover:bg-slate-400" key={ index }>{ f }</li>
-                ))}
-            </ul>
+        <div onClick={onShow} className="max-w-xs py-4 bg-white mix-blend-multiply bg-opacity-70 rounded-md border-[1px] transition-all duration-150 ease-in-out border-sky-500 absolute right-0 top-16 text-black shadow-lg">
+            <div onClick={handleClick}>
+                <h3 className="text-sm font-mono text-green-700 px-4">Favorite Locations:</h3>
+                {fav.length !== 0 ? (
+                    <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
+                        {fav.map((f, index) => (
+                            <li
+                                onClick={() => setEndpoint(f)}
+                                className="hover:bg-slate-400"
+                                key={index}
+                            >
+                                {f}
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p className="text-sm font-thin text-center py-3">no data in there</p>
+                )}
+            </div>
         </div>
     );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,12 +11,14 @@ export default function Header ()
     <header className="fixed w-full top-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
       <nav className="container flex items-center justify-between py-6">
         <Logo />
-        <div className="flex items-center gap-4 relative">
+        <div className="flex items-center gap-4">
           <Search />
-          <Fav onShow={ () => setSHow( !show ) } />
+          <div className="relative">
+            <Fav onShow={ () => setSHow( !show ) } />
           {
             show && <FavModal onShow={()=>setSHow(!show)}/>
           }
+          </div>
         </div>
       </nav>
     </header>

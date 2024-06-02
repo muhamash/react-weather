06d0cb8ee987f28bad610808114b654decc8d063
muhamash <muhammad-ashraful@outlook.com>
2024-06-02 17:29:27 +0600

    name resolved

--- src/components/hooks/useLocalStorage.jsx
+++ src/components/hooks/useLocalStorage.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
 
-const useLocalStorage = (storageValue, storageKey) => {
-    const getStoredValue = () => {
-        try {
-            const item = localStorage.getItem(storageKey);
-            return item ? JSON.parse(item) : storageValue;
-        } catch (error) {
-            console.error("Error parsing JSON from localStorage", error);
+const useLocalStorage = ( storageValue, storageKey ) =>
+{
+    const getStoredValue = () =>
+    {
+        try
+        {
+            const item = localStorage.getItem( storageKey );
+            return item ? JSON.parse( item ) : storageValue;
+        }
+        catch ( error )
+        {
+            console.error( "Error parsing JSON from localStorage", error );
             return storageValue;
         }
     };
 
-    const [value, setValue] = React.useState(getStoredValue);
+    const [ value, setValue ] = React.useState( getStoredValue );
 
-    React.useEffect(() => {
-        try {
-            localStorage.setItem(storageKey, JSON.stringify(value));
-        } catch (error) {
-            console.error("Error setting item in localStorage", error);
+    React.useEffect( () =>
+    {
+        try
+        {
+            localStorage.setItem( storageKey, JSON.stringify( value ) );
+        } catch ( error )
+        {
+            console.error( "Error setting item in localStorage", error );
         }
-    }, [value, storageKey]);
+    }, [ value, storageKey ] );
 
-    return [value, setValue];
+    return [ value, setValue ];
 };
 
 export default useLocalStorage;

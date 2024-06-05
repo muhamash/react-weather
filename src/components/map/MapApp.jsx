730eb448a083519ef41c8bc1c5ca2ee5d95fcdd1
muhamash <muhammad-ashraful@outlook.com>
2024-06-06 03:58:12 +0600

    moving for version 3

--- src/components/map/MapApp.jsx
+++ src/components/map/MapApp.jsx
@@ -1,65 +1,40 @@
-/* eslint-disable no-undef */
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
 import { useRain } from '../hooks/useRain';
 import LeafletMap from './Leaflet';
 
-// const getWeatherData = async (setWeatherData) => {
-//   try {
-//     const response = await fetch("https://api.rainviewer.com/public/weather-maps.json");
-//     if (!response.ok) throw new Error('Network response was not ok');
-//     const json = await response.json();
-//     console.log('rainviewer', json)
-//     // setWeatherData(json);
-//   } catch (error) {
-//     console.error('Fetching weather data failed:', error);
-//     setWeatherData({});
-//   }
-// };
+const MapApp = ({ lat, lon }) => {
+    const { rain } = useRain();
+    const [weatherData, setWeatherData] = useState(null);
+    const [weatherStep, setWeatherStep] = useState(0);
 
-const MapApp = ( { lat, lon } ) =>
-{
-  console.log( lon, lat )
-  const {rain} = useRain()
-  const [weatherData, setWeatherData] = useState(null);
-  const [weatherStep, setWeatherStep] = useState(0);
+    useEffect(() => {
+        setWeatherData(rain);
+    }, [rain]);
 
-  console.log(rain.radar.past)
-  useEffect( () =>
-  {
-    
-    setWeatherData( rain );
-    // console.log(weatherData.radar.past.length )
-  }, [] );
-  // console.log(weatherData)
+    useEffect(() => {
+        if (weatherData && weatherData.radar && weatherData.radar.past) {
+            const numSteps = weatherData.radar.past.length;
+            const interval = setInterval(() => {
+                setWeatherStep((prevStep) => (prevStep + 1) % numSteps);
+            }, 1000);
 
-  useEffect( () =>
-  {
-    if ( weatherData )
-    {
-      const numSteps = weatherData.radar.past.length;
-      const interval = setInterval( () =>
-      {
-        setWeatherStep( ( prevStep ) => ( prevStep + 1 ) % numSteps );
-      }, 1000 );
-      
-      // console.log('interval',interval)
-      return () => clearInterval( interval );
-    }
-  }, [ weatherData ] );
+            return () => clearInterval(interval);
+        }
+    }, [weatherData]);
 
-  return (
-    <div className="w-[300px] h-[200px]">
-      <LeafletMap
-        lat={lat}
-        lon={lon}
-        rainData={weatherData}
-        weatherStep={weatherStep}
-      />
-    </div>
-  );
+    return (
+        <div className="w-full h-full">
+            {weatherData && (
+                <LeafletMap
+                    lat={lat}
+                    lon={lon}
+                    rainData={weatherData}
+                    weatherStep={weatherStep}
+                />
+            )}
+        </div>
+    );
 };
 
 export default MapApp;
